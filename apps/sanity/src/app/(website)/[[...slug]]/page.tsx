23fc81fe5f106834179f16a984428d8b57d9643d
Jhonattan Benitez Ballesteros <40676789+jhonattanbenitez@users.noreply.github.com>
2025-03-04 10:05:41 -0500

    Initial commit

---
+++ apps/sanity/src/app/(website)/[[...slug]]/page.tsx
@@ -0,0 +1,81 @@
+import { type Metadata } from "next";
+import type { OpenGraph } from "next/dist/lib/metadata/types/opengraph-types";
+import { notFound } from "next/navigation";
+
+import { PAGE_BY_SLUG_QUERY } from "@/lib/api/queries";
+import { sanityFetch } from "@/lib/live";
+import { generateStaticSlugs } from "@/lib/loader/generateStaticSlugs";
+import { urlForOpenGraphImage } from "@/lib/utils";
+import Page from "@/components/Page";
+
+type Props = {
+  params: Promise<{ slug: string[] | undefined }>;
+};
+
+const getSlug = (params: { slug: string[] | undefined }) => {
+  // TODO: check why this happens
+  if (params.slug?.[0] === "_next") return null;
+
+  return params.slug ? `/${params.slug.join("/")}` : "/";
+};
+
+export async function generateMetadata(props: Props): Promise<Metadata> {
+  const params = await props.params;
+  const slug = getSlug(params);
+
+  if (!slug) {
+    return {};
+  }
+
+  const { data: page } = await sanityFetch({
+    query: PAGE_BY_SLUG_QUERY,
+    params: { slug },
+  });
+
+  const ogImage = urlForOpenGraphImage(page?.ogImage);
+  const openGraph: OpenGraph = {
+    title: page?.seoTitle,
+    description: page?.seoDescription,
+    images: ogImage ? [ogImage] : [],
+  };
+
+  const canonical = new URL(
+    `${process.env.NEXT_PUBLIC_DOMAIN as string}${slug === "/" ? "" : slug}`,
+  ).toString();
+
+  return {
+    alternates: {
+      canonical,
+    },
+    metadataBase: new URL(process.env.NEXT_PUBLIC_DOMAIN as string),
+    title: page?.seoTitle,
+    description: page?.seoDescription,
+    openGraph,
+    // keywords: page?.seoKeywords,
+    robots: page?.robots === "index" ? { index: true } : { index: false },
+  };
+}
+
+export async function generateStaticParams() {
+  return generateStaticSlugs("page");
+}
+
+export default async function PageSlugRoute(props: Props) {
+  const params = await props.params;
+  const slug = getSlug(params);
+
+  if (!slug) {
+    notFound();
+  }
+
+  const { data: page } = await sanityFetch({
+    query: PAGE_BY_SLUG_QUERY,
+    params: { slug },
+  });
+
+  if (!page) {
+    notFound();
+  }
+
+  return <Page data={page} />;
+}
---
+++ apps/storyblok/src/components/SectionContainer/index.tsx
@@ -0,0 +1,56 @@
+import { storyblokEditable } from "@storyblok/react/rsc";
+
+import { cn } from "@shared/ui";
+
+import type { ISectionContainerProps } from "./types";
+
+export default function SectionContainer({
+  children,
+  blok,
+  className,
+}: ISectionContainerProps) {
+  const {
+    _uid,
+    paddingX,
+    paddingY,
+    marginTop,
+    marginBottom,
+    maxWidth,
+    backgroundColor,
+    backgroundImage,
+  } = blok;
+
+  const style = backgroundImage?.filename
+    ? {
+        background: `url(${backgroundImage.filename}) no-repeat center/cover`,
+      }
+    : {};
+
+  return (
+    <section
+      {...storyblokEditable(blok)}
+      className={cn("overflow-x-hidden", className, backgroundColor, {
+        "bg-bgColor": backgroundColor && backgroundColor !== "none",
+        "mt-0": marginTop === "none",
+        "mb-0": marginBottom === "none",
+        "mt-sectionBase": marginTop === "base",
+        "mb-sectionBase": marginBottom === "base",
+        "mt-sectionLg": marginTop === "lg",
+        "mb-sectionLg": marginBottom === "lg",
+      })}
+      id={_uid}
+      style={style}
+    >
+      <div
+        className={cn("mx-auto px-4 py-8", {
+          "px-0": paddingX === "none",
+          "py-0": paddingY === "none",
+          "max-w-screen-xl": maxWidth === "base",
+          "max-w-screen-sm": maxWidth === "small",
+        })}
+      >
+        {children}
+      </div>
+    </section>
+  );
+}
